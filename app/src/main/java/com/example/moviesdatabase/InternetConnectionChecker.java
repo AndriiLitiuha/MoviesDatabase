b1e9ba339c80b06eb2f480a5d3e486e8789fd1f4
AndriiLitiuha <32611habib@gmail.com>
2021-12-12 02:17:31 +0200

    InternetConnectionChecker added

---
+++ app/src/main/java/com/example/moviesdatabase/InternetConnectionChecker.java
@@ -0,0 +1,41 @@
+package com.example.moviesdatabase;
+
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import java.net.InetAddress;
+
+public class InternetConnectionChecker {
+    public boolean isInternetAvailable() {
+        try {
+            InetAddress ipAddr = InetAddress.getByName("google.com");
+            return !ipAddr.equals("");
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean hasConnection(final Context context)
+    {
+        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifiInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (wifiInfo != null && wifiInfo.isConnected())
+        {
+            return true;
+        }
+        wifiInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if (wifiInfo != null && wifiInfo.isConnected())
+        {
+            return true;
+        }
+        wifiInfo = cm.getActiveNetworkInfo();
+        if (wifiInfo != null && wifiInfo.isConnected())
+        {
+            return true;
+        }
+        return false;
+    }
+}
--- app/src/main/java/com/example/moviesdatabase/MainActivity.java
+++ app/src/main/java/com/example/moviesdatabase/MainActivity.java
@@ -8,13 +8,23 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button retry;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button retry = findViewById(R.id.retryButt);
-        retry.setOnClickListener(view -> Toast.makeText(getApplicationContext(), getString(R.string.toast_retry), Toast.LENGTH_SHORT).show());
+        retry = findViewById(R.id.retryButt);
+    }
+
+   public void checker(View view) {
+
+        /*
+        InternetConnectionChecker checker = new InternetConnectionChecker();*/
+        if (InternetConnectionChecker.hasConnection(getApplicationContext())){
+            Toast.makeText(getApplicationContext(), getString(R.string.toast_retry), Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(getApplicationContext(), getString(R.string.toast_please_connect), Toast.LENGTH_SHORT).show();
+        }
     }
 }
